e0c817166b9847c26d8653415158bdaafdb8df18
Adammuzammil <mm592678@gmail.com>
2024-04-24 13:27:58 +0530

    Authentication logic

--- server/index.js
+++ server/index.js
@@ -2,10 +2,15 @@ import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 import connectDB from "./db/connectToDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import {
+  getMe,
+  login,
+  logout,
+  signup,
+} from "../controllers/auth.controller.js";
+import { VerifyUser } from "../middleware/VerifyUser.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", login);
+router.post("/logout", logout);
+router.route("/me").get(VerifyUser, getMe);
+// router.get("/me", VerifyUser, getMe);
 
 export default router;
